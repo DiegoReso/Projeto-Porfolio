70d042b2ff09cc145d9b3a702b879674fe99ed30
Diego Reis <diegoreso@me.com>
2023-04-20 10:06:24 -0300

    update js carousel, css e index

--- js/components/main-carousel.js
+++ js/components/main-carousel.js
@@ -5,16 +5,18 @@
 const  buttonRight = document.querySelector('.button-arrow.-right');
 const  buttonLeft = document.querySelector('.button-arrow.-left');
 const elements = document.querySelector('.elements');
-let pixels = 0;
+let pixels = 0
 
 buttonLeft.addEventListener('click', function(){
+    pixels = pixels -100
     elements.style =`transform: translateX(${pixels}px`;
-    pixels = pixels - 100;
+    
 })
 
 buttonRight.addEventListener('click', function(){
-    elements.style =`transform: translateX(${pixels}px)`;
     pixels = pixels + 100;
+    elements.style =`transform: translateX(${pixels}px)`;
+    
 })
 
 
---
+++ readme.md
@@ -0,0 +1,7 @@
+Exemplo de portfolio do curso HTML e CSS Feliz do professor Marco Bruno no youtube.
+
+
+Usando apenas html css e duas funções javascript para mover o carrosel.
+
+
+
